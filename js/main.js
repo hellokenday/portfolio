728b7ce654f3fd1df2c6be4639baaadfaec44c9b
Ken <hellokenday@gmail.com>
2018-06-27 17:29:12 +0100

    fix lazy load and pause bootstrap carousels

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
 $( document ).ready(function() {
   
-//  initLazyLoad();
   pauseCarousels();
 });
 
 // Bootstrap carousel
 function pauseCarousels() {
   
-  $('.carousel').carousel('pause');
+  $('.carousel').carousel({   
+    interval: false 
+  });
 }
 
-// Lazy load images
-function initLazyLoad(w, d){
-  
+// Lazyload images
+(function(w, d){
 	var b = d.getElementsByTagName('body')[0];
 	var s = d.createElement("script"); s.async = true;
-	var v = !("IntersectionObserver" in w) ? "8.8.0" : "10.7.0";
+	var v = !("IntersectionObserver" in w) ? "8.9.0" : "10.8.0";
 	s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
-	w.lazyLoadOptions = {}; // Your options here. See "recipes" for more information about async.
+	w.lazyLoadOptions = {
+    elements_selector: ".lazy-load"
+  }; // Your options here. See "recipes" for more information about async.
 	b.appendChild(s);
-}(window, document) ;
-
-var myLazyLoad = new LazyLoad({
-  
-    elements_selector: ".lazy-load"
-});
-
-(function() {
-  new LazyLoad({
-    elements_selector: ".lazy-load"
-  });
-}());
+}(window, document));
