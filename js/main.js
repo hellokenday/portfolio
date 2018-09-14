01df31c63081c536ce2dc30b2beb85b3b98ccbbe
Ken <hellokenday@gmail.com>
2018-09-14 17:32:05 +0100

    fix lazyload and navbar hide

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $( document ).ready(function() {
   fadeInPages();
   pauseCarousels();
   hideNavbarOnScroll();
+  
+  window.LazyLoad = new LazyLoad({
+    elements_selector: ".lazy-load",
+  });
+  window.LazyLoad.update();
 });
 
 function fadeInPages() {
@@ -33,10 +38,10 @@ function hideNavbarOnScroll() {
 (function(w, d){
 	var b = d.getElementsByTagName('body')[0];
 	var s = d.createElement("script"); s.async = true;
-	var v = !("IntersectionObserver" in w) ? "8.9.0" : "10.8.0";
+	var v = !("IntersectionObserver" in w) ? "8.15.2" : "10.17.0";
 	s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
 	w.lazyLoadOptions = {
-    elements_selector: ".lazy-load"
+    elements_selector: ".lazy-load",
   }; // Your options here. See "recipes" for more information about async.
 	b.appendChild(s);
 }(window, document));
