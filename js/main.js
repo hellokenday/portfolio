a69895b9ed600e439e1a317032c4626c96dbe4c4
Ken Day <hello@ken-day.co.uk>
2019-05-09 16:42:29 -0700

    implement tab component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   
-  checkNavbar();
+  checkComponents();
   fadeInPages();
   pauseCarousels();
   scrollToAnchor();
@@ -11,7 +11,7 @@ $( document ).ready(function() {
   window.LazyLoad.update();
 });
 
-function checkNavbar() {
+function checkComponents() {
   
   if ($("nav").hasClass("navbar")) {
     
@@ -104,6 +104,37 @@ function scrollToAnchor() {
     }
   });
 }
+ 
+
+// Tabbed images
+
+var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("image-tab");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+
 
 // Lazyload images
 (function(w, d){
